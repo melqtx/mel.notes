1a8b519f79222e8c4a8536c61c3e0b2128faf098
v <vaibhavmalik1127@gmail.com>
2025-04-11 18:19:44 +0530

    Quartz sync: Apr 11, 2025, 6:19 PM

--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -14,7 +14,7 @@ import fs from "node:fs/promises"
 import chalk from "chalk"
 
 const defaultOptions: SocialImageOptions = {
-  colorScheme: "lightMode",
+  colorScheme: "darkMode",
   width: 1200,
   height: 630,
   imageStructure: defaultImage,
